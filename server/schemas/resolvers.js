4a164f605d2acfaeb4ce8e38a1431eb889e734c6
Ryan Carey <ryaanski18@yahoo.com>
2022-02-21 19:29:31 -0500

    completed resolvers. unsure if they work yet or not

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,68 @@
+const { User, Book } = require("../models");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return Profile.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+
+  Mutation: {
+    login: async (parent, { email, password }) => {
+      const profile = await Profile.findOne({ email });
+
+      if (!profile) {
+        throw new AuthenticationError("No profile with this email found!");
+      }
+
+      const correctPw = await profile.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect password!");
+      }
+
+      const token = signToken(profile);
+      return { token, profile };
+    },
+
+    addUser: async (parent, { name, email, password }) => {
+      const user = await User.create({ name, email, password });
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    saveBook: async (parent, { user, body }, context) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: user._id },
+          {
+            $addToSet: { savedBooks: body },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      }
+      // If user attempts to execute this mutation and isn't logged in, throw an error
+      throw new AuthenticationError("You need to be logged in!");
+    },
+
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        return Profile.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: bookId } },
+          { new: true }
+        );
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+};
+
 module.exports = resolvers;
