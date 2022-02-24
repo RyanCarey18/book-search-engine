f10f42ec29dcc7f5f92a4906d5f6b15c50af5227
Ryan Carey <ryaanski18@yahoo.com>
2022-02-23 23:24:44 -0500

    removed unnecessaray code that was making lint angry

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -12,7 +12,7 @@ const SignupForm = () => {
     email: "",
     password: "",
   });
-  const [addUser, { error, data }] = useMutation(ADD_USER);
+  const [addUser] = useMutation(ADD_USER);
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -62,15 +62,13 @@ const SearchBooks = () => {
     }
   };
 
-  const [saveBook, { error, data }] = useMutation(SAVE_BOOK);
+  const [saveBook] = useMutation(SAVE_BOOK);
 
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
-    const myBookId = bookToSave.bookId;
-    const myTitle = bookToSave.title;
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
